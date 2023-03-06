241602de62d672dfe14d58942e37c40c53fe0c0b
MisterZig <ziggynewton@gmail.com>
2023-03-06 17:41:41 +0100

    idk

--- MarsRoverImages/test.ts
+++ MarsRoverImages/test.ts
@@ -1,11 +1,11 @@
-import {Axios, AxiosResponse} from 'axios';
+import {Axios, AxiosResponse, AxiosRequestConfig} from 'axios';
 
 const baseUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
 
 const axios = new Axios();
 
-function getPhotos(sol:number, page:number = 1, camera:string | undefined = undefined): Promise<AxiosResponse> {
-    const config = {
+function getPhotos(sol:number, page:number = 1, camera?:string, url:string = baseUrl): Promise<AxiosResponse> {
+    const config: AxiosRequestConfig = {
         params: {
             sol: sol,
             api_key: 'DEMO_KEY',
@@ -14,8 +14,7 @@ function getPhotos(sol:number, page:number = 1, camera:string | undefined = unde
         }
     };
 
-
-    return axios.get(baseUrl, config)
+    return axios.get(url, config);
 }
 
 async function savePhotosFromResponse(response_promise: Promise<AxiosResponse>) {
@@ -23,4 +22,24 @@ async function savePhotosFromResponse(response_promise: Promise<AxiosResponse>) 
     console.log(`Total photos: ${response.data.total_photos},\nFrom cameras: ${JSON.stringify(response.data.cameras)}`);
 }
 
-savePhotosFromResponse(getPhotos(1000, 1));
+
+async function getPhotos2() {
+    const apiKey = "DEMO_KEY";
+    const apiUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
+    const config: AxiosRequestConfig = {
+      params: {
+        sol: 1000, // Martian sol (day) on which the photos were taken
+        camera: "fhaz", // Camera name
+        api_key: apiKey,
+      },
+    };
+    try {
+      const response = await axios.get(apiUrl, config);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+}
+getPhotos2();
+
+//savePhotosFromResponse(getPhotos(1000, 1));
